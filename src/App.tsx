37dd8fc782ca78138d08f57b3b56147860ee98c9
emmashek <shek.em@northeastern.edu>
2024-09-16 16:43:08 -0400

    A1 3.4.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
-import Labs from './Lab';
+import Kanbas from './Kanbas';
+import Labs from './Labs';
+import { HashRouter, Route, Routes, Navigate} from "react-router-dom";
+
 
 function App() {
   return (
+    <HashRouter>
     <div>
-      <h1>Welcome to Web Dev! </h1>
-      <Labs />
+    <Routes>
+        <Route path="/" element={<Navigate to="Labs" />} />          
+        <Route path="/Labs/*" element={<Labs />} />
+        <Route path="/Kanbas/*" element={<Kanbas />} />
+      </Routes>
     </div>
+    </HashRouter>
+
     
   );
 }
-
 export default App;
