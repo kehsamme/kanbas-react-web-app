112fd57d59f5ecbf20ebadea073ff1fff3aafb71
emmashek <shek.em@northeastern.edu>
2024-09-16 02:16:38 -0400

    Lab 2.11.7

--- src/Lab/Lab1/index.tsx
+++ src/Lab/Lab1/index.tsx
@@ -9,9 +9,227 @@ export default function Lab1() {
             <div id="wd-p-tag">
                 <h4>Paragraph Tag</h4>
                 <p id="wd-p-1">
-This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag
-            </p>
+    This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag
+                </p>
+                <p id="wd-p-2">
+    This is the first paragraph. The paragraph tag is used to format
+    vertical gaps between long pieces of text like this one.
+                </p>
+                <p id="wd-p-3">
+    This is the second paragraph. Even though there is a deliberate white
+    gap between the paragraph above and this paragraph, by default
+    browsers render them as one contiguous piece of text as shown here on
+    the right.
+                </p>
+                <p id="wd-p-4">
+    This is the third paragraph. Wrap each paragraph with the paragraph
+    tag to tell browsers to render the gaps.
+                </p>
         </div>
+            <div id="wd-lists">
+                <h4>List Tags</h4>
+                <h5>Ordered List Tag</h5>
+                How to make pancakes:
+                <ol id="wd-pancakes">
+                    <li>Mix dry ingredients.</li>
+                    <li>Add wet ingredients.</li>
+                    <li>Stir to combine.</li>
+                    <li>Heat a skillet or griddle.</li>
+                    <li>Pour batter onto the skillet.</li>
+                    <li>Cook until bubbly on top.</li>
+                    <li>Flip and cook the other side.</li>
+                    <li>Serve and enjoy!</li>
+                </ol>
+                My favorite recipe:
+                <ol id="wd-my-favorite-recipe">
+                    <li>Crack two eggs.</li>
+                    <li>Whip with chopsticks until solid yellow.</li>
+                    <li>Heat up pan.</li>
+                    <li>Add oil.</li>
+                    <li>Add eggs and lower to medium heat.</li>
+                    <li>Once the egg starts bubbling begin to scramble with spatula.</li>
+                    <li>Scramble to desired consistency.</li>
+                    <li>Add cheese, salt, and pepper.</li>
+                    <li>Serve and enjoy!</li>
+                </ol>
+                <h5>Unordered List Tag</h5>
+                My favorite books (in no particular order)
+                <ul id="wd-my-books">
+                    <li>Dune</li>
+                    <li>Lord of the Rings</li>
+                    <li>Ender's Game</li>
+                    <li>Red Mars</li>
+                    <li>The Forever War</li>
+                </ul>
+                Your favorite books (in no particular order)
+                <ul id="wd-your-books">
+                    <li>The Midnight Library</li>
+                    <li>Born a Crime</li>
+                    <li>Before the Coffee Gets Cold</li>
+                    <li>Crying in HMart</li>
+                </ul>
+            </div>
+            <div id="wd-table"> 
+            <h4>Table Tag</h4>
+            <table border={1} width="100%">
+                <thead>
+                    <tr>
+                    <th>Quiz</th>
+                    <th>Topic</th>
+                    <th>Date</th>
+                    <th>Grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Q1</td>
+                        <td>HTML</td>
+                        <td>2/3/21</td>
+                        <td>85</td>
+                    </tr>
+                    <tr>
+                        <td>Q2</td>
+                        <td>CSS</td>
+                        <td>2/10/21</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>Q3</td>
+                        <td>JavaScript</td>
+                        <td>2/17/21</td>
+                        <td>95</td>
+                    </tr>
+                    <tr>
+                        <td>Q4</td>
+                        <td>Node.js</td>
+                        <td>2/22/21</td>
+                        <td>95</td>
+                    </tr>
+                    <tr>
+                        <td>Q5</td>
+                        <td>React</td>
+                        <td>2/25/21</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>Q6</td>
+                        <td>TypeScript</td>
+                        <td>2/28/21</td>
+                        <td>80</td>
+                    </tr>
+                    <tr>
+                        <td>Q7</td>
+                        <td>C++</td>
+                        <td>3/1/21</td>
+                        <td>85</td>
+                    </tr>
+                    <tr>
+                        <td>Q8</td>
+                        <td>Data Science</td>
+                        <td>3/10/21</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td>Q9</td>
+                        <td>Machine Learning</td>
+                        <td>3/17/21</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>Q10</td>
+                        <td>Python</td>
+                        <td>3/23/21</td>
+                        <td>95</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={3}>Average</td>
+                        <td>90.5</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+        <div id="wd-images">
+            <h4>Image tag</h4>
+            Loading an image from the internet:
+            <br />
+            <img id="wd-starship"
+                width="400px"
+            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+            />
+            <br />
+            Loading a local image:
+            <br />
+            <img id="wd-teslabot" src="Desktop/teslabot.jpg" height="200px" />
+        </div>
+        <h4>Text boxes</h4>
+        <label>Biography:</label><br/>
+        <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur 
+    adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+        <h5 id="wd-buttons">Buttons</h5>
+        <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
+        Hello World!
+        </button>
+        <h5>File upload</h5>
+        <input id="wd-upload" type="file"/>
+        
+        <h5 id="wd-radio-buttons">Radio buttons</h5>
+        <label>Favorite movie genre:</label><br />
+
+        <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+        <label htmlFor="wd-radio-comedy">Comedy</label><br />
+
+        <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+        <label htmlFor="wd-radio-drama">Drama</label><br />
+
+        <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+        <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+
+        <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+        <label htmlFor="wd-radio-fantasy">Fantasy</label>
+
+        <h5 id="wd-checkboxes">Checkboxes</h5>
+        <label>Favorite movie genre:</label><br/>
+
+        <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+        <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+
+        <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+        <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+
+        <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+        <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+
+        <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+        <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+        <h4 id="wd-dropdowns">Dropdowns</h4>
+
+        <h5>Select one</h5>
+        <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+        <select id="wd-select-one-genre">
+        <option value="COMEDY">Comedy</option>
+        <option value="DRAMA">Drama</option>
+        <option selected value="SCIFI">
+            Science Fiction</option>
+        <option value="FANTASY">Fantasy</option>
+        </select>
+
+        <h5>Select many</h5>
+        <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+        <select id="wd-select-many-genre" multiple>
+        <option selected value="COMEDY">Comedy</option>
+        <option value="DRAMA">Drama</option>
+        <option selected value="SCIFI">
+            Science Fiction</option>
+        <option value="FANTASY">Fantasy</option>
+        </select>
+        
             <h3>Exercise 1.2</h3>
             <h3>Exercise 1.3</h3>
         </div>
