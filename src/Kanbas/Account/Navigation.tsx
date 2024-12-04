9de839563e54778996c22e0ebfd50fac3fb62c3c
emmashek <shek.em@northeastern.edu>
2024-12-04 17:43:56 -0500

    A6 4.1.2

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -2,21 +2,33 @@ import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
+  
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? [{ path: "/Kanbas/Account/Profile", label: "Profile" }] 
-  : [
-      { path: "/Kanbas/Account/Signin", label: "Signin" }, 
-      { path: "/Kanbas/Account/Signup", label: "Signup" }
-    ];
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   const { pathname } = useLocation();
+
+
+  // const links = currentUser ? [{ path: "/Kanbas/Account/Profile", label: "Profile" }] 
+  // : [
+  //     { path: "/Kanbas/Account/Signin", label: "Signin" }, 
+  //     { path: "/Kanbas/Account/Signup", label: "Signup" }
+  //   ];
 
 
   return (
       <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-        {links.map((link) => (
+        {/* {links.map((link) => (
         <Link key={link.path} to={link.path} className={`list-group-item bg-white text-left border-0
               ${pathname.includes(link.label) ? "active border border-0" : "text-danger border border-0"}`}>
           {link.label}
-        </Link>))}
+        </Link>))} */}
+        {links.map((link) => (
+       <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item ${active(link)}`}> {link} </Link>
+        ))}
+        {currentUser && currentUser.role === "FACULTY" && (
+       <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )}
+       <hr />
+
     </div>
 );}
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -4,6 +4,30 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+export const findUsersByRole = async (role: string) => {
+  const response = await
+    axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete( `${USERS_API}/${userId}` );
+  return response.data;
+};
+
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  return response.data;
+};
+
+
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post( `${USERS_API}/signin`, credentials );
   return response.data;
@@ -41,6 +65,14 @@ export const createCourse = async (course: any) => {
   const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
   return data;
 };
+
+export const createUser = async (user: any) => {
+  console.log('Payload to be sent create user:', user); // Log the payload
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
+};
+
+
 
   
   
